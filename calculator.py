87fda4823b67000ba07e91912a7e8a67e9b82a99
VinceBro <vbreault99@gmail.com>
2019-04-21 20:46:38 -0400

    maudite belle journée

--- calculator.py
+++ calculator.py
@@ -1,8 +1,9 @@
 import math, argparse, numpy
 def parseall():
     parser = argparse.ArgumentParser(description="Calculator")
-    parser.add_argument("-a", "--angle", metavar="Float", type=float,dest='angle', help="L'angle à convertir, il faut définir l'unité après la valeur (deg ou rad)")
-    parser.add_argument("-sp", "--scalar-product",metavar='Tuple' ,nargs='+',type= tuple, dest='sproduct', help='Calcule le produit scalaire, prends des vecteurs en entrée')
+    parser.add_argument("-a", "--angle", metavar="Float",default=None, dest='angle', help="L'angle à convertir, il faut définir l'unité après la valeur (deg ou rad)")
+    parser.add_argument("-dp", "--dotproduct",nargs='+',default=None, type= tuple, dest='dproduct', help='Calcule le produit scalaire, prends des vecteurs en entrée')
+    parser.add_argument("-q", "--quadratic",nargs='+',default=None, dest='quad',type=float,help="Calcule les zéros d'une fonction quadratique")
     return parser.parse_args()
 
 
@@ -10,10 +11,21 @@ class Calculator():
     def __init__(self):
         self.memory = 0
         self.pi = math.pi
-    def scalarproduct(self, vector_list):
+    def getcrossproduct(self):
+        pass
+    def getdotproduct(self):
+        pass
+    def dotproduct(self, vector_list):
         return sum(i*j for i,j in zip(vector_list[0],  vector_list[1]))
     def crossproduct(self, vector_list):
         return numpy.cross(vector_list[0], vector_list[1])
+    def getangle(self, angle):
+        if "rad" in str(angle):
+            print("Angle : {} is {} deg".format(angle,calc.radtodeg(angle)))
+        elif "deg" in str(angle):
+            print("Angle : {} is {} rad".format(angle, calc.degtorad(angle)))
+        else:
+            print("y dude")
     def degtorad(self, angle):
         return(2*self.pi*self.extractfloat(angle)/360)
     def radtodeg(self, angle):
@@ -29,21 +41,18 @@ class Calculator():
             return rect_angle[0]*self.pi
         else:
             return rect_angle[0]
-    def findzeros(self, a, b, c):
-        if (b**2-4*a*c) > 0:
-            return ((-b+math.sqrt(b**2-4*a*c))/2*a, (-b-math.sqrt(b**2-4*a*c))/2*a)
+    def getzeros(self, a, b, c):
+        if (b**2-4*a*c) >= 0:
+            print("Zeros are : {} and {}".format((-b+math.sqrt(b**2-4*a*c))/2*a, (-b-math.sqrt(b**2-4*a*c))/2*a))
         else:
-            return None
+            print("Complex answer")
 
 if __name__ == "__main__":
     ARGS=parseall()
-    print(ARGS.sproduct)
-
     calc = Calculator()
-    angle = input("Entre ton angle mon excellent ami\n")
-    if "rad" in str(angle):
-        print("Angle : {} is {} deg".format(angle,calc.radtodeg(angle)))
-    elif "deg" in str(angle):
-        print("Angle : {} is {} rad".format(angle, calc.degtorad(angle)))
-    else:
-        print("y dude")
+    if ARGS.angle is not None:
+        calc.getangle(ARGS.angle)
+    elif ARGS.dproduct is not None:
+        calc.getdotproduct()
+    elif ARGS.quad is not None:
+        calc.getzeros(ARGS.quad[0], ARGS.quad[1], ARGS.quad[2])
